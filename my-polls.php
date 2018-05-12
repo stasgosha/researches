274a6cf8e2e5fd41a16a7e8efb794aeecc648397
Stas Goshovsky <stasgo@one-v.co.il>
2018-05-12 21:00:51 +0300

    - "my polls" page design update

--- my-polls.php
+++ my-polls.php
@@ -1,4 +1,4 @@
-<?php $page_title = "Мои исследования" ?>
+<?php $page_title = "Мои опросы" ?>
 <?php include('page-parts/header.inc.php'); ?>
 
 <div class="main-section">
@@ -13,33 +13,34 @@
 				<div class="page-header">
 					<h1><?= $page_title ?></h1>
 				</div>
-				<div class="interview-filters-list">
+				<div class="interview-filters-list stretch-items">
 					<div class="interview-filter current">
 						<div class="label">Все</div>
 						<div class="count">10</div>
 					</div>
 					<div class="interview-filter">
-						<div class="label">Активные</div>
+						<div class="label">Новые</div>
 						<div class="count">5</div>
 					</div>
 					<div class="interview-filter">
-						<div class="label">Черновики</div>
+						<div class="label">Незавершенные</div>
 						<div class="count">3</div>
 					</div>
 					<div class="interview-filter">
-						<div class="label">В ожидании</div>
+						<div class="label">Завершенные</div>
 						<div class="count">2</div>
 					</div>
 					<div class="interview-filter">
-						<div class="label">Завершенные</div>
+						<div class="label">Просроченные</div>
 						<div class="count">0</div>
 					</div>
-					<div class="interview-filter dark">
+					<!-- Add new poll block -->
+					<!-- <div class="interview-filter dark">
 						<div class="add-new">
 							<span class="plus">+</span>
 							<span class="text">Создать <br>опрос</span>
 						</div>
-					</div>
+					</div> -->
 				</div>
 				<div class="sort-filters-section">
 					<div class="caption">
@@ -78,20 +79,39 @@
 				</div>
 				<div class="researches-cards-list-wrapper">
 					<div class="researches-cards-list">
+						<!-- Layout of the card without old item goes below this card in the for() loop -->
 						<div class="research-card">
 							<div class="card-overlay"></div>
 							<a href="#" class="card-image" style="background-image: url()"></a>
-							<div class="card-name">
-								<h5><a href="#">Название</a></h5>
+							<div class="card-timer">
+								<div class="timer-block">
+									<div class="timer-icon">
+										<i class="icon icon-clock"></i>
+									</div>
+									<div class="timer-text">
+										<div class="label">Таймер</div>
+										<div class="countdown">01:25:48</div>
+										<!-- Таймер <br>отключен -->
+									</div>
+								</div>
 							</div>
-							<div class="card-section">
+							<div class="card-name">
+								<h5><a href="#">Длинное название опроса в несколько строк</a></h5>
+							</div>
+							<div class="card-section space-around">
 								<div class="item">
 									<div class="card-stats">
-										<span class="count">10</span> ответов
+										<div class="stats-label">Вопросов</div>
+										<span class="stats-count">10</span>
 									</div>
 								</div>
 								<div class="item">
-									<div class="card-actions">
+									<div class="card-stats">
+										<div class="stats-label">Заработок</div>
+										<span class="stats-count">5,25$</span>
+									</div>
+									<!-- OLD -->
+									<!-- <div class="card-actions">
 										<div class="action-icons-list">
 											<a href="#">
 												<i class="icon icon-edit"></i>
@@ -103,11 +123,18 @@
 												<i class="icon icon-trash"></i>
 											</a>
 										</div>
-									</div>
+									</div> -->
 								</div>
 							</div>
 							<div class="card-section">
 								<div class="item">
+									<div class="card-text">
+										<p>Ограничение по времени: <br><strong>2ч 45мин</strong></p>
+										<p>Среднее время прохождения: <br><strong>1ч 38мин</strong></p>
+									</div>
+								</div>
+								<!-- OLD -->
+								<!-- <div class="item">
 									<div class="card-toggle">
 										<div class="switch">
 											<input type="checkbox" id="card1">
@@ -116,18 +143,11 @@
 									</div>
 								</div>
 								<div class="item">
-									<div class="timer-block">
-										<div class="timer-icon">
-											<i class="icon icon-clock"></i>
-										</div>
-										<div class="timer-text">
-											<div class="label">Таймер</div>
-											<div class="countdown">01:25:48</div>
-										</div>
-									</div>
-								</div>
+									
+								</div> -->
 							</div>
-							<div class="card-hidden-content">
+							<!-- OLD -->
+							<!-- <div class="card-hidden-content">
 								<div class="card-section">
 									<div class="item">
 										<div class="card-stats">
@@ -151,79 +171,44 @@
 										</a>
 									</div>
 								</div>
-							</div>
+							</div> -->
 						</div>
 						<?php for ($i = 0; $i < 5; $i++): ?>
 							<div class="research-card">
 								<div class="card-overlay"></div>
 								<a href="#" class="card-image" style="background-image: url()"></a>
-								<div class="card-name">
-									<h5><a href="#">Название</a></h5>
+								<div class="card-timer">
+									<div class="timer-block">
+										<div class="timer-icon">
+											<i class="icon icon-clock"></i>
+										</div>
+										<div class="timer-text">
+											<div class="label">Таймер <br>отключен</div>
+										</div>
+									</div>
 								</div>
-								<div class="card-section">
+								<div class="card-name">
+									<h5><a href="#">Название опроса</a></h5>
+								</div>
+								<div class="card-section space-around">
 									<div class="item">
 										<div class="card-stats">
-											<span class="count">10</span> ответов
+											<div class="stats-label">Вопросов</div>
+											<span class="stats-count">10</span>
 										</div>
 									</div>
 									<div class="item">
-										<div class="card-actions">
-											<div class="action-icons-list">
-												<a href="#">
-													<i class="icon icon-edit"></i>
-												</a>
-												<a href="#">
-													<i class="icon icon-stats"></i>
-												</a>
-												<a href="#">
-													<i class="icon icon-trash"></i>
-												</a>
-											</div>
+										<div class="card-stats">
+											<div class="stats-label">Заработок</div>
+											<span class="stats-count">5,25$</span>
 										</div>
 									</div>
 								</div>
 								<div class="card-section">
 									<div class="item">
-										<div class="card-toggle">
-											<div class="switch">
-												<input type="checkbox" id="card<?= $i + 2; ?>">
-												<label for="card<?= $i + 2; ?>"></label>
-											</div>
-										</div>
-									</div>
-									<div class="item">
-										<div class="timer-block">
-											<div class="timer-icon">
-												<i class="icon icon-clock"></i>
-											</div>
-											<div class="timer-text">
-												<div class="label">Таймер <br>отключен</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="card-hidden-content">
-									<div class="card-section">
-										<div class="item">
-											<div class="card-stats">
-												<span class="count">100</span> коинов потрачено
-											</div>
-										</div>
-									</div>
-									<div class="card-section">
-										<div class="card-share">
-											<a href="#" class="item">
-												<span class="item-icon">
-													<i class="icon icon-print"></i>
-												</span>
-												<span class="item-text">Распечетать <br>анкету</span>
-											</a>
-											<a href="#" class="item">
-												<span class="item-icon">
-													<i class="icon icon-cloud"></i>
-												</span>
-												<span class="item-text">Загрузить <br>в облако</span>
-											</a>
+										<div class="card-text">
+											<p>Ограничение по времени: <br><strong>2ч 45мин</strong></p>
+											<p>Среднее время прохождения: <br><strong>1ч 38мин</strong></p>
 										</div>
 									</div>
 								</div>
--- page-parts/sidebar.inc.php
+++ page-parts/sidebar.inc.php
@@ -1,12 +1,10 @@
 <div class="sidebar">
 	<ul class="sidebar-nav">
 		<li><a href="index.php">Экспресс-панель</a></li>
-		<li><a href="my-researches.php">Мои исследования</a></li>
-		<li><a href="#">Заказать исследования</a></li>
-		<li><a href="#">Мои рефералы</a></li>
-		<li><a href="#">Мои настройки</a></li>
-		<li><a href="#">Страница компании</a></li>
-		<li><a href="#">Тех поддержка</a></li>
-		<li><a href="#">Оплата</a></li>
+		<li><a href="my-polls.php">Мои опросы</a></li>
+		<li><a href="ext-interests.php">Расширение интересов</a></li>
+		<li><a href="refferalls.php">Мои рефералы</a></li>
+		<li><a href="withdraw.php">Вывод средств</a></li>
+		<li><a href="settings.php">Мои настройки</a></li>
 	</ul>
 </div>
