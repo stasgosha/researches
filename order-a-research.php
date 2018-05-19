fe38282e34e1410cc5d186feca45a4aac8e224fd
Stas Goshovsky <stasgosha@yandex.ru>
2018-05-19 13:29:55 +0300

    - research order page

---
+++ order-a-research.php
@@ -0,0 +1,67 @@
+<?php $page_title = "Заказать исследование" ?>
+<?php include('page-parts/header.inc.php'); ?>
+
+<div class="main-section">
+	<?php include('page-parts/sidebar.inc.php'); ?>
+	<div class="main-content">
+		<div class="main-content-inner">
+			<div class="page-content page-text">
+				<ul class="breadcrumbs">
+					<li><a href="index.php">Главная</a></li>
+					<li><?= $page_title ?></li>
+				</ul>
+				<div class="page-header">
+					<h1><?= $page_title ?></h1>
+				</div>
+				<div class="small-title">
+					<p>Для заказа исследования заполните форму ниже <br>и мы свяжемся с вами в течение 1 часа</p>
+				</div>
+				<div class="form order-research-form bold-labels">
+					<div class="form-row">
+						<div class="form-label">Короткое описание исследования</div>
+						<div class="form-field">
+							<textarea></textarea>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-label">Охват аудитории (количество человек)</div>
+						<div class="form-field">
+							<div class="from-to-fields">
+								<div class="item from">
+									<input type="tel" placeholder="от">
+								</div>
+								<div class="item to">
+									<input type="tel" placeholder="до">
+								</div>
+							</div>
+							<div class="margin-bottom-20px"></div>
+							<div class="select-range">
+								<img src="img/temp/range.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-label">Ваше имя</div>
+						<div class="form-field">
+							<input type="text">
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-label">Номер телефона</div>
+						<div class="form-field">
+							<input type="tel">
+						</div>
+					</div>
+					<div class="form-row">
+						<p>Если Вы являетесь представителем организации, <br>укажите, пожалуйста, ее название</p>
+					</div>
+					<div class="form-row">
+						<div class="flex-container align-center">
+							<button class="btn btn-default margin-right-40px">Отправить</button>
+							<button class="btn btn-inline">Отменить</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include('page-parts/footer.inc.php'); ?>
--- page-parts/sidebar.inc.php
+++ page-parts/sidebar.inc.php
@@ -2,7 +2,7 @@
 	<ul class="sidebar-nav">
 		<li><a href="index.php">Экспресс-панель</a></li>
 		<li><a href="my-researches.php">Мои исследования</a></li>
-		<li><a href="order-research.php">Заказать исследования</a></li>
+		<li><a href="order-a-research.php">Заказать исследования</a></li>
 		<li><a href="my-refferals.php">Мои реффералы</a></li>
 		<li><a href="my-settings.php">Мои настройки</a></li>
 		<li><a href="conpany-page.php">Страница компании</a></li>
